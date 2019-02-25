e9d5ec66a775ec27f3dee63d492e6f4b2d325912
Kate F <follett.katie@gmail.com>
2019-02-25 17:50:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,10 @@
+# Whether number 0 is divisible by number 1.
 
+# Inputs: It takes in 2 numbers
+# Outputs: A boolean saying whether number0 is divisible by number1.
+#def divisible?(number0, number1)
+  #(number0 % number1) == 0
+#end
 
 
 
